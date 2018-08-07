7f6a18e14615906657815c4e622a339af35a3f18
vanmith <39536546+vanmith@users.noreply.github.com>
2018-08-07 23:36:35 +0100

    Start

---
+++ header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Britannia Car Finance</title>
+
+	<!-- Stylesheet -->
+	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/reset.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/main.css">
+
+	<?php wp_head(); ?>
+
+</head>
+<body>
+	<div id="top-bar">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6">
+					
+				</div>
+				<div class="col-lg-6">
+					
+				</div>
+			</div>
+		</div>
+	</div>
+	<header>
+		<div id="header-inner" class="container">
+			<div class="row">
+				<div class="col-lg-4">
+					<a href="/"><img src="<?php echo get_template_directory_uri(); ?>/img/logo.png"></a>
+				</div>
+				<div class="col-lg-4">
+					
+				</div>
+				<div class="col-lg-4">
+					
+				</div>
+			</div>
+		</div>
+	</header>
+	<div class="container">
+		
+	</div>
+
