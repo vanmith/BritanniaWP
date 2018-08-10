c5e0ec9036ee2f1c6eff87982405b35f221957cc
vanmith <39536546+vanmith@users.noreply.github.com>
2018-08-10 17:22:49 +0100

    Work Changes

--- header.php
+++ header.php
@@ -6,6 +6,7 @@
 	<!-- Stylesheet -->
 	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/reset.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/main.css">
 
 	<?php wp_head(); ?>
@@ -16,7 +17,8 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-lg-6">
-					
+					<a href="/" class="top-car-nav top-car-nav-active"><i class="fas fa-car"></i> Car Deals</a>
+					<a href="#" class="top-car-nav"><i class="fas fa-shuttle-van"></i> Van Deals</a>
 				</div>
 				<div class="col-lg-6">
 					<ul class="list-inline pull-right">
@@ -35,10 +37,10 @@
 		<div id="header-inner" class="container">
 			<div class="row">
 				<div class="col-lg-4">
-					<a href="/"><img src="<?php echo get_template_directory_uri(); ?>/img/logo.png"></a>
+					<a href="/"><img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" id="logo"></a>
 				</div>
 				<div class="col-lg-4">
-					
+
 				</div>
 				<div class="col-lg-4">
 					<span id="phone-number" class="pull-right">Call: <a href="tel:01614407272">0161 440 7272</a></span>
@@ -47,11 +49,19 @@
 		</div>
 	</header>
 	<div id="nav-bar">
-		<nav id="main-nav">
-			<ul class="second-nav">
-				<li><a href=""></a></li>
-			</ul>
-		</nav>
+		<div class="container">
+			<div class="row">
+				<nav id="main-nav">
+					<ul class="second-nav list-inline">
+						<li><a href="">Manufacturers <i class="fas fa-angle-right"></i></a></li>
+						<li><a href="">Model Search <i class="fas fa-angle-right"></i></a></li>
+						<li><a href="">Body Type Search <i class="fas fa-angle-right"></i></a></li>
+						<li><a href="">Budget Search <i class="fas fa-angle-right"></i></a></li>
+						<li><a href="">Advanced Search <i class="fas fa-angle-right"></i></a></li>
+					</ul>
+				</nav>
+			</div>
+		</div>
 	</div>
 	<div class="container">
 		
--- index.php
+++ index.php
@@ -1,4 +1,96 @@
 <?php get_header(); ?>
 
+<div class="container-fluid" id="top-strap-ad">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-8">
+				<h1 id="main-heading">View the best car lease offers</h1>
+				<span id="main-subheading">Our very latest car leasing deals</span><br>	
+				<button id="main-button">View Our Specials</button>
+			</div>
+			<div class="col-lg-4">
+				
+			</div>
+		</div>
+	</div>
+</div>
+<div class="container-fluid" id="top-strap-ad-bottom-bar">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-4">
+				<span id="top-strap-ad-bottom-bar-pros"><i class="fas fa-thumbs-up"></i> Price Match Promise</span>
+			</div>
+			<div class="col-lg-4">
+				<span id="top-strap-ad-bottom-bar-pros"><i class="fas fa-thumbs-up"></i> Price Match Promise</span>
+			</div>
+			<div class="col-lg-4">
+				<span id="top-strap-ad-bottom-bar-pros"><i class="fas fa-thumbs-up"></i> Price Match Promise</span>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="container" id="main-content-wrapper">
+	<div class="row">
+		<div class="col-lg-7">
+			<h2>Drive a new car for less</h2>
+			<p>We are a multi-brand specialist in personal car finance. We offer a comprehensive nationwide service with deals starting from under £100 per month.We’ve been supplying and delivering cars all around the UK for 20 years. Our experienced, knowledgeable and friendly sales team will help find a deal suitable for your budget and requirements. </p>
+			<p>We are a multi-brand specialist in personal car finance. We offer a comprehensive nationwide service with deals starting from under £100 per month.We’ve been supplying and delivering cars all around the UK for 20 years. Our experienced, knowledgeable and friendly sales team will help find a deal suitable for your budget and requirements. </p>
+			<h5>Check out the latest Audi Q8</h5>
+			<iframe width="560" height="315" src="https://www.youtube.com/embed/NZfYC4lgEFc" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+		</div>
+		<div class="col-lg-1">
+
+		</div>
+		<div class="col-lg-4">
+			<div id="timeline">
+				<span class="timeline-title">Price Match Promise</span>
+				<p class="timeline-text">Completed your order and found a better price elsewhere? Let us know and we'll be happy to send you a cheque for £50.</p>
+			</div>
+			<div id="timeline">
+				<span class="timeline-title">Price Match Promise</span>
+				<p class="timeline-text">Completed your order and found a better price elsewhere? Let us know and we'll be happy to send you a cheque for £50.</p>
+			</div>
+			<div id="timeline">
+				<span class="timeline-title">Price Match Promise</span>
+				<p class="timeline-text">Completed your order and found a better price elsewhere? Let us know and we'll be happy to send you a cheque for £50.</p>
+			</div>
+			<div id="timeline">
+				<span class="timeline-title">Price Match Promise</span>
+				<p class="timeline-text">Completed your order and found a better price elsewhere? Let us know and we'll be happy to send you a cheque for £50.</p>
+			</div>
+			<div id="timeline">
+				<span class="timeline-title">Price Match Promise</span>
+				<p class="timeline-text">Completed your order and found a better price elsewhere? Let us know and we'll be happy to send you a cheque for £50.</p>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-lg-6" id="special-offers">
+			<div class="special-offers-wrapper">
+				<span class="special-offers-title">Special Offers</span><br>
+				<span class="special-offers-title-sub">View Our Special Offers</span>
+			</div>
+		</div>
+		<div class="col-lg-6" id="about-us">
+			<div class="special-offers-wrapper-two">
+				<span class="special-offers-title-two">About Us</span><br>
+				<span class="special-offers-title-sub-two">Learn More About Us</span>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-lg-4">
+			
+		</div>
+		<div class="col-lg-4">
+			
+		</div>
+		<div class="col-lg-4">
+			
+		</div>
+	</div>
+</div>
 
 <?php get_footer(); ?>
