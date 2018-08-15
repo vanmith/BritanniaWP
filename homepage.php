4432fb9951dce880eec513a9ed911236b0edca2a
vanmith <39536546+vanmith@users.noreply.github.com>
2018-08-15 16:39:57 +0100

    Another one

---
+++ homepage.php
@@ -0,0 +1,396 @@
+<?php 
+/*
+Template Name: Homepage
+*/ 
+?>
+
+<?php get_header(); ?>
+
+<div class="container-fluid" id="top-strap-ad">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-8">
+				<h1 id="main-heading">View the best car lease offers</h1>
+				<span id="main-subheading">Our very latest car leasing deals</span><br>	
+				<button id="main-button">View Our Specials</button>
+			</div>
+			<div class="col-lg-4">
+				
+			</div>
+		</div>
+	</div>
+</div>
+<div class="container-fluid" id="top-strap-ad-bottom-bar">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-4">
+				<div class="bottom-bar-pros">
+					<span class="bottom-bar-pros-title">Price Match Promise</span><br>
+					<span class="bottom-bar-pros-sub">We will beat any quote you get no matter what.</span>
+				</div>
+			</div>
+			<div class="col-lg-4">
+				<div class="bottom-bar-pros">
+					<span class="bottom-bar-pros-title">Price Match Promise</span><br>
+					<span class="bottom-bar-pros-sub">We will beat any quote you get no matter what.</span>
+				</div>
+			</div>
+			<div class="col-lg-4">
+				<div class="bottom-bar-pros">
+					<span class="bottom-bar-pros-title">Price Match Promise</span><br>
+					<span class="bottom-bar-pros-sub">We will beat any quote you get no matter what.</span>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="container" id="main-content-wrapper">
+	<div class="row">
+		<div class="col-lg-7">
+			<h2>Drive a new car for less</h2>
+			<p>We are a multi-brand specialist in personal car finance. We offer a comprehensive nationwide service with deals starting from under £100 per month.We’ve been supplying and delivering cars all around the UK for 20 years. Our experienced, knowledgeable and friendly sales team will help find a deal suitable for your budget and requirements. </p>
+			<p>We are a multi-brand specialist in personal car finance. We offer a comprehensive nationwide service with deals starting from under £100 per month.We’ve been supplying and delivering cars all around the UK for 20 years. Our experienced, knowledgeable and friendly sales team will help find a deal suitable for your budget and requirements. </p>
+			<h5>Check out the latest Audi Q8</h5>
+			<iframe width="560" height="315" src="https://www.youtube.com/embed/NZfYC4lgEFc" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+		</div>
+		<div class="col-lg-1">
+
+		</div>
+		<div class="col-lg-4">
+			<div id="timeline">
+				<span class="timeline-title">Price Match Promise</span>
+				<p class="timeline-text">Completed your order and found a better price elsewhere? Let us know and we'll be happy to send you a cheque for £50.</p>
+			</div>
+			<div id="timeline">
+				<span class="timeline-title">Price Match Promise</span>
+				<p class="timeline-text">Completed your order and found a better price elsewhere? Let us know and we'll be happy to send you a cheque for £50.</p>
+			</div>
+			<div id="timeline">
+				<span class="timeline-title">Price Match Promise</span>
+				<p class="timeline-text">Completed your order and found a better price elsewhere? Let us know and we'll be happy to send you a cheque for £50.</p>
+			</div>
+			<div id="timeline">
+				<span class="timeline-title">Price Match Promise</span>
+				<p class="timeline-text">Completed your order and found a better price elsewhere? Let us know and we'll be happy to send you a cheque for £50.</p>
+			</div>
+			<div id="timeline">
+				<span class="timeline-title">Price Match Promise</span>
+				<p class="timeline-text">Completed your order and found a better price elsewhere? Let us know and we'll be happy to send you a cheque for £50.</p>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="container-fluid" id="five-car-section">
+	<div class="row">
+		<div class="col-lg-6" id="special-offers">
+			<div class="special-offers-wrapper">
+				<span class="special-offers-title">Special Offers</span><br>
+				<span class="special-offers-title-sub">View Our Special Offers</span>
+			</div>
+		</div>
+		<div class="col-lg-6" id="about-us">
+			<div class="special-offers-wrapper-two">
+				<span class="special-offers-title-two">About Us</span><br>
+				<span class="special-offers-title-sub-two">Learn More About Us</span>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-lg-4" id="about-leasing">
+			<div class="about-leasing-wrapper">
+				<span class="about-leasing-wrapper-title">All About Leasing</span><br>
+				<span class="about-leasing-wrapper-sub">Learn all you need to about leasing a car here</span>
+			</div>
+		</div>
+		<div class="col-lg-4" id="van-leasing">
+			<div class="van-leasing-wrapper">
+				<span class="van-leasing-wrapper-title">Van Lease Deals</span><br>
+				<span class="van-leasing-wrapper-sub">We can help you lease a van for business or personal use</span>
+			</div>
+		</div>
+		<div class="col-lg-4" id="electric-leasing">
+			<div class="electric-leasing-wrapper">
+				<span class="electric-leasing-wrapper-title">Electric Cars</span><br>
+				<span class="electric-leasing-wrapper-sub">Go electric with one of our amazing electric car lease deals</span>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="container" id="home-about-section">
+	<div class="row">
+		<div class="col-lg-12">
+			<h2 class="about-leasing-heading">About Car Leasing</h2>
+			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+			<h4>Test heading</h4>
+			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+			<h4>Test heading</h4>
+			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+			<h4>Test heading</h4>
+			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+			<h4>Test heading</h4> 
+			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+
+			<button id="main-button">Learn More</button>
+		</div>
+	</div>
+</div>
+<div class="container" id="home-manufacts">
+		<div class="row">
+			<div class="col-lg-12 manulink-title">
+				<h3>Manufactors we work with</h3>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-1 manulink">
+				<a href="">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/logos/alfa_romeo.png">
+					<p>Alfa Romeo</p>
+				</a>
+			</div>
+			<div class="col-lg-1 manulink">
+				<a href="">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/logos/audi.png">
+					<p>Audi</p>
+				</a>
+			</div>
+			<div class="col-lg-1 manulink">
+				<a href="">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/logos/bmw.png">
+					<p>BMW</p>
+				</a>
+			</div>
+			<div class="col-lg-1 manulink">
+				<a href="">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/logos/citroen.png">
+					<p>Citroen</p>
+				</a>
+			</div>
+			<div class="col-lg-1 manulink">
+				<a href="">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/logos/fiat.png">
+					<p>Fiat</p>
+				</a>
+			</div>
+			<div class="col-lg-1 manulink">
+				<a href="">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/logos/ford.png">
+					<p>Ford</p>
+				</a>
+			</div>
+			<div class="col-lg-1 manulink">
+				<a href="">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/logos/honda.png">
+					<p>Honda</p>
+				</a>
+			</div>
+			<div class="col-lg-1 manulink">
+				<a href="">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/logos/hyundai.png">
+					<p>Hyundai</p>
+				</a>
+			</div>
+			<div class="col-lg-1 manulink">
+				<a href="">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/logos/jaguar.png">
+					<p>Jaguar</p>
+				</a>
+			</div>
+			<div class="col-lg-1 manulink">
+				<a href="">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/logos/kia.png">
+					<p>Kia</p>
+				</a>
+			</div>
+			<div class="col-lg-1 manulink">
+				<a href="">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/logos/land_rover.png">
+					<p>Land Rover</p>
+				</a>
+			</div>
+			<div class="col-lg-1 manulink">
+				<a href="">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/logos/lexus.png">
+					<p>Lexus</p>
+				</a>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-1 manulink">
+				<a href="">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/logos/mazda.png">
+					<p>Mazda</p>
+				</a>
+			</div>
+			<div class="col-lg-1 manulink">
+				<a href="">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/logos/merc.png">
+					<p>Mercedes Benz</p>
+				</a>
+			</div>
+			<div class="col-lg-1 manulink">
+				<a href="">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/logos/mini.png">
+					<p>Mini</p>
+				</a>
+			</div>
+			<div class="col-lg-1 manulink">
+				<a href="">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/logos/mitsubishi.png">
+					<p>Mitsubishi</p>
+				</a>
+			</div>
+			<div class="col-lg-1 manulink">
+				<a href="">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/logos/nissan.png">
+					<p>Nissan</p>
+				</a>
+			</div>
+			<div class="col-lg-1 manulink">
+				<a href="">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/logos/peugeot.png">
+					<p>Peugeot</p>
+				</a>
+			</div>
+			<div class="col-lg-1 manulink">
+				<a href="">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/logos/porsche.png">
+					<p>Porsche</p>
+				</a>
+			</div>
+			<div class="col-lg-1 manulink">
+				<a href="">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/logos/renault.png">
+					<p>Renault</p>
+				</a>
+			</div>
+			<div class="col-lg-1 manulink">
+				<a href="">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/logos/seat.png">
+					<p>Seat</p>
+				</a>
+			</div>
+			<div class="col-lg-1 manulink">
+				<a href="">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/logos/smart.png">
+					<p>Smart</p>
+				</a>
+			</div>
+			<div class="col-lg-1 manulink">
+				<a href="">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/logos/skoda.png">
+					<p>Skoda</p>
+				</a>
+			</div>
+			<div class="col-lg-1 manulink">
+				<a href="">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/logos/tesla.png">
+					<p>Tesla</p>
+				</a>
+			</div>
+			
+		</div>
+		<div class="row">
+			<div class="col-lg-4">
+				
+			</div>
+			<div class="col-lg-1 manulink">
+				<a href="">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/logos/toyota.png">
+					<p>toyota</p>
+				</a>
+			</div>
+			<div class="col-lg-1 manulink">
+				<a href="">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/logos/vauxhall.png">
+					<p>Vauxhall</p>
+				</a>
+			</div>
+			<div class="col-lg-1 manulink">
+				<a href="">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/logos/volvo.png">
+					<p>Volvo</p>
+				</a>
+			</div>
+			<div class="col-lg-1 manulink">
+				<a href="">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/logos/vw.png">
+					<p>VW</p>
+				</a>
+			</div>
+			<div class="col-lg-4">
+				
+			</div>
+		</div>
+</div>
+<div class="container-fluid change-background">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12 faqs">
+				<h2>Frequently Asked Questions</h2>
+				<div class="faq">
+					<div class="faq_question"><h3>I'm interested in leasing but I don't know anything about it. Can you advise me?</h3></div>
+					<div class="faq_answer_container">
+						<div class="faq_answer"><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p></div>
+					</div>		
+				</div>
+				<div class="faq">
+					<div class="faq_question"><h3>I've always bought my car, why should I lease one?</h3></div>
+					<div class="faq_answer_container">
+						<div class="faq_answer"><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p></div>
+					</div>		
+				</div>
+				<div class="faq">
+					<div class="faq_question"><h3>Your prices seem very low, what's the catch?</h3></div>
+					<div class="faq_answer_container">
+						<div class="faq_answer"><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p></div>
+					</div>		
+				</div>
+				<div class="faq">
+					<div class="faq_question"><h3>I can't see the exact model I want to lease, what do I do?</h3></div>
+					<div class="faq_answer_container">
+						<div class="faq_answer"><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p></div>
+					</div>		
+				</div>
+				<div class="faq">
+					<div class="faq_question"><h3>Are the cars you supply brand new cars?</h3></div>
+					<div class="faq_answer_container">
+						<div class="faq_answer"><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p></div>
+					</div>		
+				</div>
+				<a href="">View all Questions</a>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="container">
+	<div class="row">
+		<div class="col-lg-12 testimonials">
+			<h2>Testimonials</h2>
+		</div>
+	</div>
+</div>
+<div class="container-fluid change-background">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12 faqs">
+				<h2>Our Services</h2>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-3">
+				
+			</div>
+			<div class="col-lg-3">
+				
+			</div>
+			<div class="col-lg-3">
+				
+			</div>
+			<div class="col-lg-3">
+				
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php get_footer(); ?> 		
