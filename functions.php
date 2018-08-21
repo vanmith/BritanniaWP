86fa33a03b77c499bf1f8cf2cca87f0e0c5dad4c
vanmith <39536546+vanmith@users.noreply.github.com>
2018-08-21 15:39:40 +0100

    acf 5

--- functions.php
+++ functions.php
@@ -1,4 +1,4 @@
-<?phpif( function_exists('acf_add_local_field_group') ):
+<?php if( function_exists('acf_add_local_field_group') ):
 
 acf_add_local_field_group(array(
 	'key' => 'group_5b7c16d944e35',
